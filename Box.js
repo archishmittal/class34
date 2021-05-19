dab035e208bc1fb43d1fd69724aaac4ec59c3075
archishmittal <75306301+archishmittal@users.noreply.github.com>
2021-05-19 17:52:43 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,39 @@
+class Box{
+
+  constructor(x,y ,width,height){
+
+  var options = {
+    restitution: 0.8,
+    friction: 1.0,
+    density: 0.04
+
+  }
+
+this.width= width;
+this.height= height;
+this.body=Bodies.rectangle(x,y,width,height,options);
+World.add (world,this.body);
+
+
+
+
+
+  }
+
+ 
+  display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+      stroke("green");
+      fill("aquamarine");
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  
+
+}
